cdbcec3806127fdaa396f38d34f5a1ee5f329667
manuels-code <101575315+manuels-code@users.noreply.github.com>
2022-06-04 22:12:09 -0500

    Create testimonial.component

---
+++ src/components/testimonial.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Section from "./layout/section.component";
+
+const Testimonial = () => {
+  return (
+    <Section>
+      <h2 className="text-center">
+        <span>Testimonial</span> What Our Customers Are Saying.
+      </h2>
+      
+    </Section>
+  );
+};
+
+export default Testimonial;
