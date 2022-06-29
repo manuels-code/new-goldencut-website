37c1c7d59eb3a72ae43b1ba7f38696ff461b69ce
manuels-code <101575315+manuels-code@users.noreply.github.com>
2022-06-29 18:37:02 -0500

    Update slider.component.jsx

--- src/components/slider.component.jsx
+++ src/components/slider.component.jsx
@@ -3,29 +3,33 @@ import TestimonialsCard from "./testimonialsCard.component";
 
 const Slider = ({ reviews }) => {
   const [position, setPosition] = useState(0);
-  const [transition, setTransition] = useState("");
+  const [transition, setTransition] = useState("2s");
   const [intervalSpeed, setIntervalSpeed] = useState(5000);
+
+  const style = {
+    transform: `translateX(-${position}00%)`,
+    transition: `${transition}`,
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (position < reviews.length) {
         setPosition(position + 1);
-        setTransition("transition");
-        setIntervalSpeed(5000);
+        setIntervalSpeed(2000)
+        setTransition('2s')
       } else {
         setPosition(0);
-        setTransition("transition-none");
-        setIntervalSpeed(0);
+        setIntervalSpeed(0)
+        setTransition('')
       }
+
       clearInterval(interval);
     }, intervalSpeed);
-  }, [position, intervalSpeed, reviews.length]);
+  }, [position,intervalSpeed, reviews.length]);
 
   return (
     <div className="overflow-hidden w-[80%] mx-auto">
-      <div
-        className={`flex -translate-x-[${position}00%] ${transition} duration-[1000ms]`}
-      >
+      <div style={style} className="flex">
         {reviews.map((review) => (
           <TestimonialsCard key={review.id} review={review} />
         ))}
