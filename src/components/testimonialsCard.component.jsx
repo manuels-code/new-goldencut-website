480fe3cb8ec945c9b7ed09a66ffa609b7631a7e7
manuels-code <101575315+manuels-code@users.noreply.github.com>
2022-06-29 13:44:27 -0500

    fixed card spacing

--- src/components/testimonialsCard.component.jsx
+++ src/components/testimonialsCard.component.jsx
@@ -2,23 +2,25 @@ import React from "react";
 
 const TestimonialsCard = ({ review }) => {
   return (
-    <div className="border rounded-md min-w-[100%] flex flex-col p-4 gap-4 justify-between h-md">
-      <div>
-        <h3>{review.review.title}</h3>
-        <p>{review.review.context}</p>
-      </div>
-
-      <div className="flex items-center gap-4">
-        <img
-          className="rounded-full w-24 h-24"
-          src={review.author.profilePhoto}
-          alt=""
-        />
+    <div className="min-w-[100%] p-2">
+      <div className="border rounded-md flex flex-col p-4 gap-4 justify-between h-md">
         <div>
-          <h4>
-            {review.author.name}
-            <span className="block">{review.author.position}</span>
-          </h4>
+          <h3>{review.review.title}</h3>
+          <p>{review.review.context}</p>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <img
+            className="rounded-full w-24 h-24"
+            src={review.author.profilePhoto}
+            alt=""
+          />
+          <div>
+            <h4>
+              {review.author.name}
+              <span className="block">{review.author.position}</span>
+            </h4>
+          </div>
         </div>
       </div>
     </div>
